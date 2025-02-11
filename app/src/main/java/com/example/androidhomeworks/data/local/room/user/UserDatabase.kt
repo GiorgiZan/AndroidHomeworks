bc458272bcd9293dac6b0c2c283e02236f8a8e09
Administrator <giorgizandarashvili7@gmail.com>
2025-02-12 02:14:19 +0400

    twenty fourth homework done needs to be optimized

--- app/src/main/java/com/example/androidhomeworks/data/local/room/user/UserDatabase.kt
+++ app/src/main/java/com/example/androidhomeworks/data/local/room/user/UserDatabase.kt
@@ -4,13 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.androidhomeworks.data.local.room.keys.RemoteKey
-import com.example.androidhomeworks.data.local.room.keys.RemoteKeyDao
 
-@Database(entities = [UserEntity::class, RemoteKey::class], version = 1, exportSchema = false)
+@Database(entities = [UserEntity::class], version = 1, exportSchema = false)
 abstract class UserDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
-    abstract fun remoteKeyDao(): RemoteKeyDao
+
 
     companion object {
         @Volatile
