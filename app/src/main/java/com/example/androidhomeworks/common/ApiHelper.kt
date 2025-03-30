938b02dc56d76493003b6fc6ef7f9eb8d6e9a966
Administrator <giorgizandarashvili7@gmail.com>
2025-03-30 14:47:01 +0400

    Revert "updated main branch"
    This reverts commit d645c54a

--- app/src/main/java/com/example/androidhomeworks/common/ApiHelper.kt
+++ app/src/main/java/com/example/androidhomeworks/common/ApiHelper.kt
@@ -3,8 +3,6 @@ package com.example.androidhomeworks.common
 
 import android.content.Context
 import com.example.androidhomeworks.domain.resource.Resource
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import retrofit2.Response
 import java.io.IOException
@@ -12,40 +10,36 @@ import java.io.IOException
 class ApiHelper(private val context: Context) {
 
 
-    fun <T> handleHttpRequest(apiCall: suspend () -> Response<T>): Flow<Resource<T>> = flow {
+    suspend fun <T> handleHttpRequest(apiCall: suspend () -> Response<T>): Resource<T> {
         if (!NetworkHelper.isNetworkAvailable(context)) {
-            emit(Resource.Error(errorMessage = "No internet connection"))
+            return Resource.Error(errorMessage = "No internet connection")
         }
 
         val response = apiCall.invoke()
-        try {
+        return try {
             if (response.isSuccessful) {
-                emit(response.body()?.let {
+                response.body()?.let {
                     Resource.Success(data = it)
-                } ?: Resource.Error(errorMessage = "Something went wrong"))
+                } ?: Resource.Error(errorMessage = "Something went wrong")
             } else {
-                emit(Resource.Error(errorMessage = response.errorBody()?.string() ?: "Error"))
+                Resource.Error(errorMessage = response.errorBody()?.string() ?: "Error")
             }
         } catch (throwable: Throwable) {
             when (throwable) {
                 is IOException -> {
-                    emit(Resource.Error(errorMessage = throwable.message ?: "IO Exception"))
+                    Resource.Error(errorMessage = throwable.message ?: "IO Exception")
                 }
 
                 is HttpException -> {
-                    emit(Resource.Error(errorMessage = throwable.message ?: "Http Exception"))
+                    Resource.Error(errorMessage = throwable.message ?: "Http Exception")
                 }
 
                 is IllegalStateException -> {
-                    emit(
-                        Resource.Error(
-                            errorMessage = throwable.message ?: "Illegal State Exception"
-                        )
-                    )
+                    Resource.Error(errorMessage = throwable.message ?: "Illegal State Exception")
                 }
 
                 else -> {
-                    emit(Resource.Error(errorMessage = throwable.message ?: "Unknown Exception"))
+                    Resource.Error(errorMessage = throwable.message ?: "Unknown Exception")
                 }
             }
 
